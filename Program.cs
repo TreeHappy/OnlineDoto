31cae728fb7e2e818a3ad872411c4c4630f297ff
TreeHappy <97783479+TreeHappy@users.noreply.github.com>
2022-12-07 16:25:23 +0000

    b

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenDotaApi;
+
+namespace OnlineDoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var SteamId = 43593603;
+            var openDota = new OpenDota();
+            var data = await openDota.Player.GetPlayerAsync(SteamId);
+
+            Console.WriteLine(data.Profile.Avatarfull);
+        }
+    }
+}
